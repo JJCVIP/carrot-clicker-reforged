8cda0212a0fe7cf7b9e3b0bd2347ec0982f3e71d
not-the <87151784+not-the@users.noreply.github.com>
2024-04-30 15:59:35 -0400

    Comment out reportWebVitals

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './compiled_styles.css';
 
 // Components
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,4 +18,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
